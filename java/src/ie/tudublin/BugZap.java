ac4b76fd6fe28a734b9e872bf42b80a918104d97
Kai Morciniec <kaimorciniec@gmail.com>
2023-02-01 15:42:43 +0000

    Add bug

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -11,6 +11,11 @@ public class BugZap extends PApplet {
 	float playerX = random(0 + playerWidth, viewWidth - playerWidth);
 	float playerY = viewHeight - playerWidth;
 
+	int bugSpeed = 50;
+	float bugWidth = 20;
+	float bugX = random(0 + bugWidth, viewWidth - bugWidth);
+	float bugY = 0;
+
 	public void settings() {
 		size(viewWidth, viewHeight);
 	}
@@ -27,9 +32,25 @@ public class BugZap extends PApplet {
 	public void draw() {
 		background(0);
 		drawPlayer(playerX, playerY, playerWidth);
+		if ((frameCount % 20) == 0) {
+			float jump = random(-bugSpeed, bugSpeed);
+			if (bugX - jump > 0 && bugX - jump < (viewWidth - bugWidth)) {
+				bugX -= jump;
+			}
+
+		}
+		if (frameCount % 60 == 0) {
+			bugY += 15;
+
+		}
+		drawBug(bugX, bugY, bugWidth);
 	}
 
 	public void drawPlayer(float x, float y, float w) {
+		rect(x, y, w, w);
+	}
+
+	public void drawBug(float x, float y, float w) {
 		rect(x, y, w, w);
 	}
 
